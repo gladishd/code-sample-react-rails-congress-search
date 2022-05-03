d076c7c356fb237223b29dc8a2cb737836783b79
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 19:36:14 -0400

    created temporary results card

--- congress_frontend/src/components/ResultsCard.js
+++ congress_frontend/src/components/ResultsCard.js
@@ -3,7 +3,34 @@ import React from 'react';
 const ResultsCard = () => {
     return(
         <div className="card">
-            
+            <h3>Your District:</h3>
+            <h4>Your Representative:</h4>
+            <p>Party:</p>
+            <p>Website:</p>
+            <p>Address:</p>
+            <p>Phone Number:</p>
+            <p>Twitter:</p>
+            <p>Facebook:</p>
+            <p>Youtube:</p>
+            <p>Birthday:</p>
+            <h4>Your Senator:</h4>
+            <p>Party:</p>
+            <p>Website:</p>
+            <p>Address:</p>
+            <p>Phone Number:</p>
+            <p>Twitter:</p>
+            <p>Facebook:</p>
+            <p>Youtube:</p>
+            <p>Birthday:</p>
+            <h4>Your Senator:</h4>
+            <p>Party:</p>
+            <p>Website:</p>
+            <p>Address:</p>
+            <p>Phone Number:</p>
+            <p>Twitter:</p>
+            <p>Facebook:</p>
+            <p>Youtube:</p>
+            <p>Birthday:</p>
         </div>
     )
 }
