71beff690e7448b19b97066d5b3f44fc62d2d5a8
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 19:02:13 -0400

    created simple form and text input

--- congress_frontend/src/components/Search.js
+++ congress_frontend/src/components/Search.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Search = () => {
     return(
         <div className="search-bar">
-
+            <form>
+                <input 
+                    type="text"
+                    className="form-entry"
+                    placeholder="Your Address Here..."
+                />
+            </form>
         </div>
     )
 }
