5147670f36d31162d60b11f0a1b112cdef42c5c4
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 18:20:05 -0400

    added get_districts route

--- congress_api/config/routes.rb
+++ congress_api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/get_districts' => 'districts#get_districts'
+
   resources :districts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
