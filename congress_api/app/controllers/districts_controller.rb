243e1f1c505a4bc2d1d5de2aa9a4ddfec1b538e6
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 18:06:35 -0400

    added temp code to test API calls

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -3,5 +3,7 @@ require 'geocodio'
 
 class DistrictsController < ApplicationController
     def get_districts
+        geocodio = Geocodio::Client.new(ENV["API_KEY"])
+        location = geocodio.geocode(['1109 N Highland St, Arlington VA'])
     end
 end
