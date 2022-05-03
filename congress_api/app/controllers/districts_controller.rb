41bfa6ddb5b6ac6dc5e0758e2ea97e20a8486f00
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 18:16:02 -0400

    migrated database

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -5,5 +5,6 @@ class DistrictsController < ApplicationController
     def get_districts
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
         location = geocodio.geocode(['1109 N Highland St, Arlington VA'])
+        byebug
     end
 end
