8a7dd241f79a2c47eb5aa72bf2d2e6e1076c32d1
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 18:51:38 -0400

    added congressional district fields to result

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -4,7 +4,7 @@ require 'geocodio'
 class DistrictsController < ApplicationController
     def get_districts
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
-        location = geocodio.geocode(['1109 N Highland St, Arlington VA'])
+        location = geocodio.geocode(['1109 N Highland St, Arlington VA'], fields: %w[cd]).best
         render json: location
     end
 end
