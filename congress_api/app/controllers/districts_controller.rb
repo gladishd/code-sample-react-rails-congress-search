8e506e76c175bafb9f3eff5251b8087f35f433c1
GoldenPavilion <rcorystine@gmail.com>
2022-05-03 18:38:06 -0400

    remove byebug - retrieves data on single address from Geocodio

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -1,10 +1,9 @@
-require 'dotenv'
+require 'dotenv-rails'
 require 'geocodio'
 
 class DistrictsController < ApplicationController
     def get_districts
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
-        byebug
         location = geocodio.geocode(['1109 N Highland St, Arlington VA'])
     end
 end
