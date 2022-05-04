b9a0b31252b148cb878fa10dcbf56e005d3a9af4
GoldenPavilion <rcorystine@gmail.com>
2022-05-04 12:00:21 -0400

    passes in addressString to geocode

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -5,7 +5,7 @@ class DistrictsController < ApplicationController
     def get_districts
         addressString = params["address"]
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
-        location = geocodio.geocode(['1109 N Highland St, Arlington VA'], fields: %w[cd]).best
+        location = geocodio.geocode([addressString], fields: %w[cd]).best
         render json: location
     end
 end
