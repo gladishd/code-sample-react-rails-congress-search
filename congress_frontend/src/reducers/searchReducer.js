430051e02a11391c03446e5abef854b9dc159bc3
GoldenPavilion <rcorystine@gmail.com>
2022-05-05 11:30:56 -0400

    set up cases

--- congress_frontend/src/reducers/searchReducer.js
+++ congress_frontend/src/reducers/searchReducer.js
@@ -4,6 +4,10 @@ const initState = {
 
 const searchReducer = (state = initState, action) => {
     switch(action.type){
+        case "UPDATE_SEARCH_FORM":
+            return action.searchData;
+        case "RESET_SEARCH_FORM":
+            return initState;
         default:
             return state;
     }
