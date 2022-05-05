b53f12878535ee6502d70451207c336d48746455
GoldenPavilion <rcorystine@gmail.com>
2022-05-05 11:12:24 -0400

    added composeEnhancer to connect to redux devtools

--- congress_frontend/src/store.js
+++ congress_frontend/src/store.js
@@ -2,5 +2,7 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
-    
+
 })
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
