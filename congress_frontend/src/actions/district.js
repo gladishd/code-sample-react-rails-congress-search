d2eb1f6c0a21ad6d4160487567277996b1bc35de
GoldenPavilion <rcorystine@gmail.com>
2022-05-06 13:42:46 -0400

    created getDistrict action to retrieve data from API

--- congress_frontend/src/actions/district.js
+++ congress_frontend/src/actions/district.js
@@ -4,3 +4,24 @@ export const setDistrict = district => {
         district
     }
 }
+
+export const getDistrict = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/get_districts", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => resp.json())
+        .then(response => {
+            if(response.error) {
+                alert(response.error)
+            } else {
+                dispatch(setDistrict(response.data))
+            }
+        })
+        .catch(console.log)
+    }
+}
