8182dfa35d617b9ae6c6d493809986724f611a62
GoldenPavilion <rcorystine@gmail.com>
2022-05-06 13:36:27 -0400

    created simple district reducer with set district case

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -3,10 +3,10 @@ require 'geocodio'
 
 class DistrictsController < ApplicationController
     def get_districts
-        addressString = params["search"]
+        ##addressString = params["search"]
+        addressString = "1600 Pennsylvania Ave Washington, DC"
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
         location = geocodio.geocode([addressString], fields: %w[cd]).best
-        byebug
         render json: location
     end
 end
---
+++ congress_frontend/src/reducers/districtReducer.js
@@ -0,0 +1,10 @@
+const districtReducer = (state = [], action) => {
+    switch(action.type){
+        case "SET_DISTRICT":
+            return action.district
+        default:
+            return state
+    }
+}
+
+export default districtReducer
