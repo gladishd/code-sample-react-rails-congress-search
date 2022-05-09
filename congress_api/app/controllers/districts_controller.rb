2aab4bde74e3c937078d28fcc9a224d854cc5130
GoldenPavilion <rcorystine@gmail.com>
2022-05-09 12:59:12 -0400

    added hard coded string for testing

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -3,7 +3,8 @@ require 'geocodio'
 
 class DistrictsController < ApplicationController
     def get_districts
-        addressString = params["search"]
+        addressString2 = params["search"]
+        addressString = "1600 Pennsylvania Ave Washington, DC"
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
         location = geocodio.geocode([addressString], fields: %w[cd]).best
         district = location.congressional_districts
