e226f519ba8908bf92aee044c69146191e1c4849
GoldenPavilion <rcorystine@gmail.com>
2022-05-09 12:57:52 -0400

    creates new District object using information from GC API

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -7,5 +7,11 @@ class DistrictsController < ApplicationController
         geocodio = Geocodio::Client.new(ENV["API_KEY"])
         location = geocodio.geocode([addressString], fields: %w[cd]).best
         district = location.congressional_districts
+        
+        newDistrict = District.create(
+            name: district[0].name,
+            number: district[0].district_number 
+        )
+        render json: newDistrict
     end
 end
