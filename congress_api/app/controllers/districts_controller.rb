e91e5621a263be818841e29fd83cbbc5de1cc37e
GoldenPavilion <rcorystine@gmail.com>
2022-05-09 13:10:45 -0400

    maps district to App

--- congress_api/app/controllers/districts_controller.rb
+++ congress_api/app/controllers/districts_controller.rb
@@ -9,10 +9,10 @@ class DistrictsController < ApplicationController
         location = geocodio.geocode([addressString], fields: %w[cd]).best
         district = location.congressional_districts
         
-        newDistrict = District.create(
-            name: district[0].name,
-            number: district[0].district_number 
-        )
-        render json: newDistrict
+        #newDistrict = District.create(
+        #    name: district[0].name,
+        #    number: district[0].district_number 
+        #)
+        render json: district
     end
 end
