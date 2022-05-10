e0cf21518ebc5645ccb64b554d10bd940b44fbb8
GoldenPavilion <rcorystine@gmail.com>
2022-05-10 11:24:50 -0400

    styled results cards

--- congress_frontend/src/components/ResultsCard.js
+++ congress_frontend/src/components/ResultsCard.js
@@ -8,37 +8,43 @@ const ResultsCard = () => {
         <div className="results">
             <h3>Your District:</h3>
             <div className="card">
-                <h4>Your Representative:</h4>
-                <p>Party:</p>
-                <p>Website:</p>
-                <p>Address:</p>
-                <p>Phone Number:</p>
-                <p>Twitter:</p>
-                <p>Facebook:</p>
-                <p>Youtube:</p>
-                <p>Birthday:</p>
+                <div className="card-body">
+                    <h5 className="card-title">Your Representative:</h5>
+                    <p className="card-text">Party:</p>
+                    <p className="card-text">Website:</p>
+                    <p className="card-text">Address:</p>
+                    <p className="card-text">Phone Number:</p>
+                    <p className="card-text">Twitter:</p>
+                    <p className="card-text">Facebook:</p>
+                    <p className="card-text">Youtube:</p>
+                    <p className="card-text">Birthday:</p>
+                </div>
             </div>
             <div className="card">
-                <h4>Your Senator:</h4>
-                <p>Party:</p>
-                <p>Website:</p>
-                <p>Address:</p>
-                <p>Phone Number:</p>
-                <p>Twitter:</p>
-                <p>Facebook:</p>
-                <p>Youtube:</p>
-                <p>Birthday:</p>
+                <div className="card-body">
+                    <h5 className="card-title">Your Senator:</h5>
+                    <p className="card-text">Party:</p>
+                    <p className="card-text">Website:</p>
+                    <p className="card-text">Address:</p>
+                    <p className="card-text">Phone Number:</p>
+                    <p className="card-text">Twitter:</p>
+                    <p className="card-text">Facebook:</p>
+                    <p className="card-text">Youtube:</p>
+                    <p className="card-text">Birthday:</p>
+                </div>
             </div>
             <div className="card">
-                <h4>Your Senator:</h4>
-                <p>Party:</p>
-                <p>Website:</p>
-                <p>Address:</p>
-                <p>Phone Number:</p>
-                <p>Twitter:</p>
-                <p>Facebook:</p>
-                <p>Youtube:</p>
-                <p>Birthday:</p>
+                <div className="card-body">
+                    <h4>Your Senator:</h4>
+                    <p>Party:</p>
+                    <p>Website:</p>
+                    <p>Address:</p>
+                    <p>Phone Number:</p>
+                    <p>Twitter:</p>
+                    <p>Facebook:</p>
+                    <p>Youtube:</p>
+                    <p>Birthday:</p>
+                </div>
             </div>
         </div>
     )
