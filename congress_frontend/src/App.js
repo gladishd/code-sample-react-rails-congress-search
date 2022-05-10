984ac15fbcd000124a4b27bb67f8d6a215ad5b26
GoldenPavilion <rcorystine@gmail.com>
2022-05-10 10:32:05 -0400

    renders Instructions in App

--- congress_frontend/src/App.js
+++ congress_frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { connect } from 'react-redux';
 import Search from './components/Search.js';
 import ResultsCard from './components/ResultsCard.js';
+import Instructions from './components/Instructions.js';
 
 function App({ district }) { 
   return (
     <div className="App">
       <h1>Congress Search</h1>
+      <Instructions />
       <Search />
       { district === '' ? '' : <ResultsCard /> }
     </div>
