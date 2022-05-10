255c2aee4f37ecb197493cb3df9790c5292bbdc9
GoldenPavilion <rcorystine@gmail.com>
2022-05-10 11:07:20 -0400

    separated each rep into different div

--- congress_frontend/src/components/ResultsCard.js
+++ congress_frontend/src/components/ResultsCard.js
@@ -5,35 +5,41 @@ import { getDistrict } from '../actions/district.js';
 const ResultsCard = () => {
 
     return(
-        <div className="card">
+        <div className="results">
             <h3>Your District:</h3>
-            <h4>Your Representative:</h4>
-            <p>Party:</p>
-            <p>Website:</p>
-            <p>Address:</p>
-            <p>Phone Number:</p>
-            <p>Twitter:</p>
-            <p>Facebook:</p>
-            <p>Youtube:</p>
-            <p>Birthday:</p>
-            <h4>Your Senator:</h4>
-            <p>Party:</p>
-            <p>Website:</p>
-            <p>Address:</p>
-            <p>Phone Number:</p>
-            <p>Twitter:</p>
-            <p>Facebook:</p>
-            <p>Youtube:</p>
-            <p>Birthday:</p>
-            <h4>Your Senator:</h4>
-            <p>Party:</p>
-            <p>Website:</p>
-            <p>Address:</p>
-            <p>Phone Number:</p>
-            <p>Twitter:</p>
-            <p>Facebook:</p>
-            <p>Youtube:</p>
-            <p>Birthday:</p>
+            <div className="card">
+                <h4>Your Representative:</h4>
+                <p>Party:</p>
+                <p>Website:</p>
+                <p>Address:</p>
+                <p>Phone Number:</p>
+                <p>Twitter:</p>
+                <p>Facebook:</p>
+                <p>Youtube:</p>
+                <p>Birthday:</p>
+            </div>
+            <div className="card">
+                <h4>Your Senator:</h4>
+                <p>Party:</p>
+                <p>Website:</p>
+                <p>Address:</p>
+                <p>Phone Number:</p>
+                <p>Twitter:</p>
+                <p>Facebook:</p>
+                <p>Youtube:</p>
+                <p>Birthday:</p>
+            </div>
+            <div className="card">
+                <h4>Your Senator:</h4>
+                <p>Party:</p>
+                <p>Website:</p>
+                <p>Address:</p>
+                <p>Phone Number:</p>
+                <p>Twitter:</p>
+                <p>Facebook:</p>
+                <p>Youtube:</p>
+                <p>Birthday:</p>
+            </div>
         </div>
     )
 }
