603b41f6d75813435f55e330d4247db8dd5fda66
GoldenPavilion <rcorystine@gmail.com>
2022-05-10 11:44:24 -0400

    indents App

--- congress_frontend/src/components/ResultsCard.js
+++ congress_frontend/src/components/ResultsCard.js
@@ -6,6 +6,7 @@ const ResultsCard = () => {
 
     return(
         <div className="results">
+            <div className="row">
             <h3>Your District:</h3>
             <div className="card">
                 <div className="card-body">
@@ -35,16 +36,17 @@ const ResultsCard = () => {
             </div>
             <div className="card">
                 <div className="card-body">
-                    <h4>Your Senator:</h4>
-                    <p>Party:</p>
-                    <p>Website:</p>
-                    <p>Address:</p>
-                    <p>Phone Number:</p>
-                    <p>Twitter:</p>
-                    <p>Facebook:</p>
-                    <p>Youtube:</p>
-                    <p>Birthday:</p>
+                    <h5 className="card-title">Your Senator:</h5>
+                    <p className="card-text">Party:</p>
+                    <p className="card-text">Website:</p>
+                    <p className="card-text">Address:</p>
+                    <p className="card-text">Phone Number:</p>
+                    <p className="card-text">Twitter:</p>
+                    <p className="card-text">Facebook:</p>
+                    <p className="card-text">Youtube:</p>
+                    <p className="card-text">Birthday:</p>
                 </div>
+            </div>
             </div>
         </div>
     )
