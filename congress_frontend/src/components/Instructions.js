0c1755a236e81553ae79bf63347b3207361c0cfb
GoldenPavilion <rcorystine@gmail.com>
2022-05-10 10:28:34 -0400

    created simple Instructions text

--- congress_frontend/src/components/Instructions.js
+++ congress_frontend/src/components/Instructions.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Instructions = () => {
     return(
         <div>
-
+            <p>Welcome! Looking for more information about your Congressional District and Representative? Search you address below. Be sure to include your City and Zip Code.</p>
         </div>
     )
 }
