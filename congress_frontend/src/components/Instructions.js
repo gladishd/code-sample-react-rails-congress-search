75a6dac26d2b547f27c6ff498dd3b4ea8e04ed57
GoldenPavilion <rcorystine@gmail.com>
2022-05-16 10:49:51 -0400

    divided paragraphs

--- congress_frontend/src/components/Instructions.js
+++ congress_frontend/src/components/Instructions.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Instructions = () => {
     return(
         <div>
-            <p>Welcome! Looking for more information about your Congressional District and Representative? Search you address below. Be sure to include your City and Zip Code.</p>
+            <p>Welcome! Looking for more information about your Congressional District and Representative?</p> 
+            <p>Search you address below. Be sure to include your City and Zip Code.</p>
         </div>
     )
 }
