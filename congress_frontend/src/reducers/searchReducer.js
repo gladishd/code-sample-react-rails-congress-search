a35e1f9d8e634a3338cdec8bc900a36dfafc460c
GoldenPavilion <rcorystine@gmail.com>
2022-05-17 11:10:34 -0400

    deleted initState

--- congress_frontend/src/reducers/searchReducer.js
+++ congress_frontend/src/reducers/searchReducer.js
@@ -1,13 +1,9 @@
-const initState = {
-    value: "1600 Pennsylvania Ave Washington, DC 20500"
-}
-
-const searchReducer = (state = initState, action) => {
+const searchReducer = (state = "", action) => {
     switch(action.type){
         case "UPDATE_SEARCH_FORM":
             return action.searchData;
         case "RESET_SEARCH_FORM":
-            return initState;
+            return state;
         default:
             return state;
     }
