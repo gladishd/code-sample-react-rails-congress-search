602c6292f9e1801f9c036ca66b4a381834f0b9cb
GoldenPavilion <rcorystine@gmail.com>
2022-05-18 12:46:55 -0400

    added padding to instructions

--- congress_frontend/src/components/Instructions.js
+++ congress_frontend/src/components/Instructions.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Instructions = () => {
     return(
-        <div>
+        <div className="instructions">
             <p>Welcome! Looking for more information about your Congressional District and Representative?</p> 
             <p>Search you address below. Be sure to include your City and Zip Code.</p>
         </div>
